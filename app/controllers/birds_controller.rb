56465cc84f50e4cafcf58b234a00b5143dbb060e
Charity Wanjiku Ngocho <ngochocharity@gmail.com>
2022-10-01 17:49:20 +0300

    commit tree 05c5f775bfa2a0fcd7b5e7fa24d3cbd9428eb2f7

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -47,4 +47,8 @@ class BirdsController < ApplicationController
     render json: { error: "Bird not found" }, status: :not_found
   end
 
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
+
 end
